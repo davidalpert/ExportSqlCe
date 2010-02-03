f2013926f8cb2c9b6414fb5a2d41fa244b786e07
SND\ErikEJ_cp <SND\ErikEJ_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-02-03 12:50:53 +0000

    work item: 11690 Generator: Flips IDENTITY_INSERT unnecessarily
    git-svn-id: https://exportsqlce.svn.codeplex.com/svn@38957 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Generator/Generator.cs
+++ Generator/Generator.cs
@@ -251,138 +251,140 @@ namespace ExportSqlCE
             // Skip rowversion column
             Int32 rowVersionOrdinal = _repository.GetRowVersionOrdinal(tableName);
             List<Column> columns = _allColumns.Where(c => c.TableName == tableName).ToList();
-            DataTable dt = _repository.GetDataFromTable(tableName, columns);
-            bool hasIdentity = _repository.HasIdentityColumn(tableName);
+            using (DataTable dt = _repository.GetDataFromTable(tableName, columns))
+            {
+                bool hasIdentity = _repository.HasIdentityColumn(tableName);
 #if V35
-            if (hasIdentity)
-            {
-                _sbScript.Append(string.Format(System.Globalization.CultureInfo.InvariantCulture, "SET IDENTITY_INSERT [{0}] ON;", tableName));
-                _sbScript.Append(Environment.NewLine);
-                _sbScript.Append(_sep);
-            }
+                if (hasIdentity && dt.Rows.Count > 0)
+                {
+                    _sbScript.Append(string.Format(System.Globalization.CultureInfo.InvariantCulture, "SET IDENTITY_INSERT [{0}] ON;", tableName));
+                    _sbScript.Append(Environment.NewLine);
+                    _sbScript.Append(_sep);
+                }
 #endif
-            var fields = new List<string>();
-            for (int iColumn = 0; iColumn < dt.Columns.Count; iColumn++)
-            {
-                fields.Add(dt.Columns[iColumn].ColumnName);
-            }
-            string scriptPrefix = GetInsertScriptPrefix(tableName, fields);
-
-            for (int iRow = 0; iRow < dt.Rows.Count; iRow++)
-            {
-                _sbScript.Append(scriptPrefix);
+                var fields = new List<string>();
                 for (int iColumn = 0; iColumn < dt.Columns.Count; iColumn++)
                 {
-
-                    //Skip rowversion column
-                    if (rowVersionOrdinal == iColumn || dt.Columns[iColumn].ColumnName.StartsWith("__sys", StringComparison.OrdinalIgnoreCase))
-                    {
-                        continue;
-                    }
-                    if (dt.Rows[iRow][iColumn] == DBNull.Value)
-                    {
-                        _sbScript.Append("null");
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(String))
-                    {
-                        _sbScript.AppendFormat("N'{0}'", dt.Rows[iRow][iColumn].ToString().Replace("'", "''"));
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(DateTime))
-                    {
-                        // see http://msdn.microsoft.com/en-us/library/ms180878.aspx#BackwardCompatibilityforDownlevelClients
-                        Column column = _allColumns.Where(c => c.TableName == tableName && c.ColumnName == dt.Columns[iColumn].ColumnName).Single();
-                        DateTime date = (DateTime)dt.Rows[iRow][iColumn];
-                        switch (column.DateFormat)
-                        {
-                            case DateFormat.None:
-                                //Datetime globalization - ODBC escape: {ts '2004-03-29 19:21:00'}
-                                _sbScript.Append("{ts '");
-                                _sbScript.Append(date.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture));
-                                _sbScript.Append("'}");
-                                break;
-                            case DateFormat.DateTime:
-                                //Datetime globalization - ODBC escape: {ts '2004-03-29 19:21:00'}
-                                _sbScript.Append("{ts '");
-                                _sbScript.Append(date.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture));
-                                _sbScript.Append("'}");
-                                break;
-                            case DateFormat.Date:
-                                _sbScript.Append("N'");
-                                _sbScript.Append(date.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture));
-                                _sbScript.Append("'");
-                                break;
-                            case DateFormat.DateTime2:
-                                _sbScript.Append("N'");
-                                _sbScript.Append(date.ToString("yyyy-MM-dd HH:mm:ss.fffffff", System.Globalization.CultureInfo.InvariantCulture));
-                                _sbScript.Append("'");
-                                break;
-                        }
-
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(DateTimeOffset))
-                    {
-                        DateTimeOffset dto = (DateTimeOffset)dt.Rows[iRow][iColumn];
-                        _sbScript.Append("N'");
-                        _sbScript.Append(dto.ToString("yyyy-MM-dd HH:mm:ss.fffffff zzz", System.Globalization.CultureInfo.InvariantCulture));
-                        _sbScript.Append("'");
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(TimeSpan))
-                    {
-                        TimeSpan ts = (TimeSpan)dt.Rows[iRow][iColumn];
-                        _sbScript.Append("N'");
-                        _sbScript.Append(ts.ToString());
-                        _sbScript.Append("'");
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(Byte[]))
-                    {
-                        Byte[] buffer = (Byte[])dt.Rows[iRow][iColumn];
-                        _sbScript.Append("0x");
-                        for (int i = 0; i < buffer.Length; i++)
-                        {
-                            _sbScript.Append(buffer[i].ToString("X2", System.Globalization.CultureInfo.InvariantCulture));
-                        }
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(Byte) || dt.Columns[iColumn].DataType == typeof(Int16) || dt.Columns[iColumn].DataType == typeof(Int32) || dt.Columns[iColumn].DataType == typeof(Int64) || dt.Columns[iColumn].DataType == typeof(Double) || dt.Columns[iColumn].DataType == typeof(Single) || dt.Columns[iColumn].DataType == typeof(Decimal))
-                    {
-                        string intString = Convert.ToString(dt.Rows[iRow][iColumn], System.Globalization.CultureInfo.InvariantCulture);
-                        _sbScript.Append(intString);
-                    }
-                    else if (dt.Columns[iColumn].DataType == typeof(Boolean))
-                    {
-                        bool boolVal = (Boolean)dt.Rows[iRow][iColumn];
-                        if (boolVal)
-                        { _sbScript.Append("1"); }
-                        else
-                        { _sbScript.Append("0"); }
-                    }
-                    else
-                    {
-                        //Decimal point globalization
-                        string value = Convert.ToString(dt.Rows[iRow][iColumn], System.Globalization.CultureInfo.InvariantCulture);
-                        _sbScript.AppendFormat("'{0}'", value.Replace("'", "''"));
-                    }
-                    _sbScript.Append(",");
+                    fields.Add(dt.Columns[iColumn].ColumnName);
                 }
-                // remove trailing comma
-                _sbScript.Remove(_sbScript.Length - 1, 1);
+                string scriptPrefix = GetInsertScriptPrefix(tableName, fields);
 
-                _sbScript.Append(");");
-                _sbScript.Append(Environment.NewLine);
-                _sbScript.Append(_sep);
-            }
+                for (int iRow = 0; iRow < dt.Rows.Count; iRow++)
+                {
+                    _sbScript.Append(scriptPrefix);
+                    for (int iColumn = 0; iColumn < dt.Columns.Count; iColumn++)
+                    {
+
+                        //Skip rowversion column
+                        if (rowVersionOrdinal == iColumn || dt.Columns[iColumn].ColumnName.StartsWith("__sys", StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;
+                        }
+                        if (dt.Rows[iRow][iColumn] == DBNull.Value)
+                        {
+                            _sbScript.Append("null");
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(String))
+                        {
+                            _sbScript.AppendFormat("N'{0}'", dt.Rows[iRow][iColumn].ToString().Replace("'", "''"));
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(DateTime))
+                        {
+                            // see http://msdn.microsoft.com/en-us/library/ms180878.aspx#BackwardCompatibilityforDownlevelClients
+                            Column column = _allColumns.Where(c => c.TableName == tableName && c.ColumnName == dt.Columns[iColumn].ColumnName).Single();
+                            DateTime date = (DateTime)dt.Rows[iRow][iColumn];
+                            switch (column.DateFormat)
+                            {
+                                case DateFormat.None:
+                                    //Datetime globalization - ODBC escape: {ts '2004-03-29 19:21:00'}
+                                    _sbScript.Append("{ts '");
+                                    _sbScript.Append(date.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture));
+                                    _sbScript.Append("'}");
+                                    break;
+                                case DateFormat.DateTime:
+                                    //Datetime globalization - ODBC escape: {ts '2004-03-29 19:21:00'}
+                                    _sbScript.Append("{ts '");
+                                    _sbScript.Append(date.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture));
+                                    _sbScript.Append("'}");
+                                    break;
+                                case DateFormat.Date:
+                                    _sbScript.Append("N'");
+                                    _sbScript.Append(date.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture));
+                                    _sbScript.Append("'");
+                                    break;
+                                case DateFormat.DateTime2:
+                                    _sbScript.Append("N'");
+                                    _sbScript.Append(date.ToString("yyyy-MM-dd HH:mm:ss.fffffff", System.Globalization.CultureInfo.InvariantCulture));
+                                    _sbScript.Append("'");
+                                    break;
+                            }
+
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(DateTimeOffset))
+                        {
+                            DateTimeOffset dto = (DateTimeOffset)dt.Rows[iRow][iColumn];
+                            _sbScript.Append("N'");
+                            _sbScript.Append(dto.ToString("yyyy-MM-dd HH:mm:ss.fffffff zzz", System.Globalization.CultureInfo.InvariantCulture));
+                            _sbScript.Append("'");
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(TimeSpan))
+                        {
+                            TimeSpan ts = (TimeSpan)dt.Rows[iRow][iColumn];
+                            _sbScript.Append("N'");
+                            _sbScript.Append(ts.ToString());
+                            _sbScript.Append("'");
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(Byte[]))
+                        {
+                            Byte[] buffer = (Byte[])dt.Rows[iRow][iColumn];
+                            _sbScript.Append("0x");
+                            for (int i = 0; i < buffer.Length; i++)
+                            {
+                                _sbScript.Append(buffer[i].ToString("X2", System.Globalization.CultureInfo.InvariantCulture));
+                            }
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(Byte) || dt.Columns[iColumn].DataType == typeof(Int16) || dt.Columns[iColumn].DataType == typeof(Int32) || dt.Columns[iColumn].DataType == typeof(Int64) || dt.Columns[iColumn].DataType == typeof(Double) || dt.Columns[iColumn].DataType == typeof(Single) || dt.Columns[iColumn].DataType == typeof(Decimal))
+                        {
+                            string intString = Convert.ToString(dt.Rows[iRow][iColumn], System.Globalization.CultureInfo.InvariantCulture);
+                            _sbScript.Append(intString);
+                        }
+                        else if (dt.Columns[iColumn].DataType == typeof(Boolean))
+                        {
+                            bool boolVal = (Boolean)dt.Rows[iRow][iColumn];
+                            if (boolVal)
+                            { _sbScript.Append("1"); }
+                            else
+                            { _sbScript.Append("0"); }
+                        }
+                        else
+                        {
+                            //Decimal point globalization
+                            string value = Convert.ToString(dt.Rows[iRow][iColumn], System.Globalization.CultureInfo.InvariantCulture);
+                            _sbScript.AppendFormat("'{0}'", value.Replace("'", "''"));
+                        }
+                        _sbScript.Append(",");
+                    }
+                    // remove trailing comma
+                    _sbScript.Remove(_sbScript.Length - 1, 1);
+
+                    _sbScript.Append(");");
+                    _sbScript.Append(Environment.NewLine);
+                    _sbScript.Append(_sep);
+                }
 #if V35
-            if (hasIdentity)
-            {
-                _sbScript.Append(string.Format(System.Globalization.CultureInfo.InvariantCulture, "SET IDENTITY_INSERT [{0}] OFF;", tableName));
-                _sbScript.Append(Environment.NewLine);
-                _sbScript.Append(_sep);
-            }
+                if (hasIdentity && dt.Rows.Count > 0)
+                {
+                    _sbScript.Append(string.Format(System.Globalization.CultureInfo.InvariantCulture, "SET IDENTITY_INSERT [{0}] OFF;", tableName));
+                    _sbScript.Append(Environment.NewLine);
+                    _sbScript.Append(_sep);
+                }
 #endif
-            if (_sbScript.Length > 10485760 && !string.IsNullOrEmpty(_outFile))
-            {
-                _fileCounter++;
-                Helper.WriteIntoFile(_sbScript.ToString(), _outFile, _fileCounter);
-                _sbScript.Remove(0, _sbScript.Length);
+                if (_sbScript.Length > 10485760 && !string.IsNullOrEmpty(_outFile))
+                {
+                    _fileCounter++;
+                    Helper.WriteIntoFile(_sbScript.ToString(), _outFile, _fileCounter);
+                    _sbScript.Remove(0, _sbScript.Length);
+                }
             }
         }
 
