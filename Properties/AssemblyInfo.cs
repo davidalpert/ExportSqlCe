50a6a9a5b3b0bd0e56669faf070a36d379457318
SND\ErikEJ_cp <SND\ErikEJ_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-04-06 17:59:02 +0000

    Build 3.0.0.1
    git-svn-id: https://exportsqlce.svn.codeplex.com/svn@45214 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.5.0.3")]
-[assembly: AssemblyFileVersion("2.5.0.3")]
+[assembly: AssemblyVersion("3.0.0.1")]
+[assembly: AssemblyFileVersion("3.0.0.1")]
