8baafae8eb77217c747d0ac93e519247350524f4
SND\ErikEJ_cp <SND\ErikEJ_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2013-01-21 08:37:56 +0000

    Updated build script
    git-svn-id: https://exportsqlce.svn.codeplex.com/svn@97829 2327b42d-5241-43d6-9e2a-de5ac946f064

--- lib/ILMExpSqlCE.cmd
+++ lib/ILMExpSqlCE.cmd
@@ -1,4 +1,5 @@
 
+echo Running ilmerge...
 c:\data\sqlce\exportsqlce\lib\ilmerge /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /out:c:\data\sqlce\ExportSqlCe.exe ..\bin\release\exportsqlce.exe QuickGraph.dll QuickGraph.Data.dll QuickGraph.GraphViz.dll
 
 c:\data\sqlce\exportsqlce\lib\ilmerge /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /out:c:\data\sqlce\ExportSqlCe40.exe ..\bin\release\exportsqlce40.exe QuickGraph.dll QuickGraph.Data.dll QuickGraph.GraphViz.dll
@@ -8,7 +9,6 @@ c:\data\sqlce\exportsqlce\lib\ilmerge /targetplatform:v4,C:\Windows\Microsoft.NE
 c:\data\sqlce\exportsqlce\lib\ilmerge /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /out:c:\data\sqlce\Export2SqlCe.exe ..\bin\release\export2sqlce.exe QuickGraph.dll QuickGraph.Data.dll QuickGraph.GraphViz.dll
 
 del c:\data\sqlce\exp*.pdb
-pause
 
 echo Copying new Scripting API files...
 
